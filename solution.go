2c9b1521cccca62a82fae91219a856541b765d3f
UnfProg <99582576+UnfProg@users.noreply.github.com>
2022-02-17 16:47:48 +0300

    upd v5

--- solution.go
+++ solution.go
@@ -19,7 +19,7 @@ var SidesCircle = 0
 func CalcSquare(sideLen float64, sides int) float64 {
 
 	if sides == 0 {
-		sqr := sideLen * math.Pi
+		sqr := sideLen * math.Pi * math.Pi
 		return sqr
 	} else if sides == 3 {
 		perimetr := 3 * sideLen
